72c2eeb79529720d87eb7231f4b5d0e7dfe0b4fd
anton-ua <antonyua23@gmail.com>
2020-05-19 20:55:42 +0300

    fix

--- src/Components/ContactsItem/ContactsItem.js
+++ src/Components/ContactsItem/ContactsItem.js
@@ -2,20 +2,20 @@ import React from "react";
 import styles from "./ContactsItem.module.css";
 import PropTypes from "prop-types";
 
-const ContactsItem = ({ name, number, id, deleteContact }) => (
-  <li>
-    <p>
-      {name}: <span>{number}</span>
-      <button
-        type="button"
-        onClick={() => deleteContact(id)}
-        className={styles.button}
-      >
-        X
-      </button>
-    </p>
-  </li>
-);
+const ContactsItem = ({ name, number, id, deleteContact }) => {
+  const del = () => deleteContact(id);
+
+  return (
+    <li>
+      <p>
+        {name}: <span>{number}</span>
+        <button type="button" onClick={del} className={styles.button}>
+          X
+        </button>
+      </p>
+    </li>
+  );
+};
 
 ContactsItem.propTypes = {
   name: PropTypes.string.isRequired,
